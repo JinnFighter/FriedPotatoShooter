7e5a24930aa914bfd017ede91a395a03fc5e828d
JinnFighter <jinnfighterwork@yandex.ru>
2022-09-04 20:45:44 +0300

    rem: callbacks

--- Assets/Scripts/Menu/RunnerWrapper.cs
+++ Assets/Scripts/Menu/RunnerWrapper.cs
@@ -1,12 +1,7 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using Fusion;
-using Fusion.Sockets;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class RunnerWrapper : MonoBehaviour, INetworkRunnerCallbacks
+public class RunnerWrapper : MonoBehaviour
 {
     private NetworkRunner _runner;
     
@@ -14,7 +9,6 @@ public class RunnerWrapper : MonoBehaviour, INetworkRunnerCallbacks
     {
         _runner = gameObject.AddComponent<NetworkRunner>();
         _runner.ProvideInput = true;
-        _runner.AddCallbacks(this);
 
         var res = await _runner.StartGame(new StartGameArgs
         {
@@ -25,81 +19,5 @@ public class RunnerWrapper : MonoBehaviour, INetworkRunnerCallbacks
         });
         
         Debug.Log(res.ShutdownReason);
-    }
-
-    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
-    {
-       
-    }
-
-    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
-    {
-
-    }
-
-    public void OnInput(NetworkRunner runner, NetworkInput input)
-    {
-
-    }
-
-    public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input)
-    {
-
-    }
-
-    public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason)
-    {
-
-    }
-
-    public void OnConnectedToServer(NetworkRunner runner)
-    {
-
-    }
-
-    public void OnDisconnectedFromServer(NetworkRunner runner)
-    {
-    }
-
-    public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token)
-    {
-    }
-
-    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason)
-    {
-
-    }
-
-    public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message)
-    {
-
-    }
-
-    public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList)
-    {
-
-    }
-
-    public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data)
-    {
-
-    }
-
-    public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken)
-    {
-    }
-
-    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ArraySegment<byte> data)
-    {
-
-    }
-
-    public void OnSceneLoadDone(NetworkRunner runner)
-    {
-
-    }
-
-    public void OnSceneLoadStart(NetworkRunner runner)
-    {
     }
 }
