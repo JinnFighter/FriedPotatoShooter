d5e34b3a6b314b37569028bfeb0d2aafd630a84d
JinnFighter <jinnfighterwork@yandex.ru>
2022-09-04 18:15:31 +0300

    add: RMB logic in NetworkInputData

--- Assets/Scripts/NetworkInputData.cs
+++ Assets/Scripts/NetworkInputData.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public struct NetworkInputData : INetworkInput
 {
     public const byte MouseButton1 = 0x01;
+    public const byte MouseButton2 = 0x02;
 
     public byte Buttons;
     public Vector3 Direction;
