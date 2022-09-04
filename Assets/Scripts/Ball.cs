ea19af74fe2fb8b54cc10f42892d20a1bbcbe2ce
JinnFighter <jinnfighterwork@yandex.ru>
2022-09-04 14:18:11 +0300

    add: Ball movement code

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,6 +1,30 @@
+using Fusion;
 using UnityEngine;
 
-public class Ball : MonoBehaviour
+public class Ball : NetworkBehaviour
 {
+    private Transform _transform;
+    [Networked] private TickTimer _life { get; set; }
 
+    public void Init()
+    {
+        _life = TickTimer.CreateFromSeconds(Runner, 5f);
+    }
+
+    private void Awake()
+    {
+        _transform = transform;
+    }
+
+    public override void FixedUpdateNetwork()
+    {
+        if (_life.Expired(Runner))
+        {
+            Runner.Despawn(Object);
+        }
+        else
+        {
+            _transform.position += 5 * _transform.forward * Runner.DeltaTime;
+        }
+    }
 }
